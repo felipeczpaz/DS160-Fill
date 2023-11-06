4540f4a4f1c4ba551c856ab27e8ee05df74fafe0
felipeczpaz <felipecezarpazzz@proton.me>
2023-11-06 18:54:32 -0300

    :tada: Project files

---
+++ script.js
@@ -0,0 +1,252 @@
+const ctl00_SiteContentPlaceHolder_FormView1_rblDisease_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblDisease_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblDisease_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblDisease_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblDisorder_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblDisorder_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblDisorder_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblDisorder_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblDruguser_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblDruguser_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblDruguser_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblDruguser_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblArrested_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblArrested_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblArrested_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblArrested_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblControlledSubstances_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblControlledSubstances_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblControlledSubstances_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblControlledSubstances_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblProstitution_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblProstitution_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblProstitution_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblProstitution_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblMoneyLaundering_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblMoneyLaundering_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblMoneyLaundering_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblMoneyLaundering_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblHumanTrafficking_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblHumanTrafficking_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblHumanTrafficking_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblHumanTrafficking_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblAssistedSevereTrafficking_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblAssistedSevereTrafficking_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblAssistedSevereTrafficking_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblAssistedSevereTrafficking_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblHumanTraffickingRelated_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblHumanTraffickingRelated_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblHumanTraffickingRelated_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblHumanTraffickingRelated_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblIllegalActivity_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblIllegalActivity_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblIllegalActivity_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblIllegalActivity_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblTerroristActivity_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblTerroristActivity_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblTerroristActivity_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblTerroristActivity_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblTerroristSupport_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblTerroristSupport_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblTerroristSupport_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblTerroristSupport_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblTerroristOrg_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblTerroristOrg_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblTerroristOrg_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblTerroristOrg_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblTerroristRel_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblTerroristRel_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblTerroristRel_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblTerroristRel_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblGenocide_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblGenocide_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblGenocide_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblGenocide_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblTorture_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblTorture_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblTorture_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblTorture_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblExViolence_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblExViolence_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblExViolence_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblExViolence_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblChildSoldier_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblChildSoldier_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblChildSoldier_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblChildSoldier_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblReligiousFreedom_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblReligiousFreedom_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblReligiousFreedom_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblReligiousFreedom_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblPopulationControls_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblPopulationControls_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblPopulationControls_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblPopulationControls_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblTransplant_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblTransplant_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblTransplant_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblTransplant_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblImmigrationFraud_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblImmigrationFraud_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblImmigrationFraud_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblImmigrationFraud_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblDeport_1 =
+  document.getElementById("ctl00_SiteContentPlaceHolder_FormView1_rblDeport_1");
+if (ctl00_SiteContentPlaceHolder_FormView1_rblDeport_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblDeport_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblRemovalHearing_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblRemovalHearing_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblRemovalHearing_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblRemovalHearing_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblFailToAttend_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblFailToAttend_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblFailToAttend_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblFailToAttend_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblVisaViolation_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblVisaViolation_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblVisaViolation_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblVisaViolation_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblChildCustody_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblChildCustody_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblChildCustody_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblChildCustody_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblVotingViolation_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblVotingViolation_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblVotingViolation_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblVotingViolation_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblRenounceExp_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblRenounceExp_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblRenounceExp_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblRenounceExp_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_FormView1_rblAttWoReimb_1 =
+  document.getElementById(
+    "ctl00_SiteContentPlaceHolder_FormView1_rblAttWoReimb_1"
+  );
+if (ctl00_SiteContentPlaceHolder_FormView1_rblAttWoReimb_1) {
+  ctl00_SiteContentPlaceHolder_FormView1_rblAttWoReimb_1.checked = true;
+}
+
+const ctl00_SiteContentPlaceHolder_UpdateButton3 = document.getElementById(
+  "ctl00_SiteContentPlaceHolder_UpdateButton3"
+);
+if (ctl00_SiteContentPlaceHolder_UpdateButton3) {
+  ctl00_SiteContentPlaceHolder_UpdateButton3.click();
+}
